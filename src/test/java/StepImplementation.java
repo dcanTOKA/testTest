f58b00b7dcc157cc251d11622714f3e4467c3de2
dcantoka <70148273+dcantoka@users.noreply.github.com>
2021-04-02 10:47:39 +0300

    Update StepImplementation.java

--- src/test/java/StepImplementation.java
+++ src/test/java/StepImplementation.java
@@ -2,7 +2,7 @@
 import com.thoughtworks.gauge.Step;
 
 public class StepImplementation {
-
+  // bos bir proje
     @Step("ornek bir adim")
     public void ornekAdim1(){
         System.out.println("Bu bir ornek step");
